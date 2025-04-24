19c4212457b71d1f91c4b14073fac17bc272066c
RINTU5500U <biswajitswain.dev@gmail.com>
2025-04-24 23:16:22 +0530

    done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -76,7 +76,7 @@ async function login(req, res) {
         }
 
         const token = jwt.sign({ id: user._id, email: user.email, phone: user.phone }, process.env.JWT_SECRET);
-        return res.status(200).json({ success: true, message: 'Logged in successfully', token });
+        return res.status(200).json({ success: true, message: 'Logged in successfully', token, user });
     } catch (error) {
         return res.status(500).json({ success: false, error: error.message });
     }
